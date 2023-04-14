2683fa7f7e5c2cec33de6e0fbd9501dc1d5c0300
Edpacca <eddie@pace.me.uk>
2023-04-14 09:41:53 +0100

    change reset function

--- src/components/settingsMenu/advanced/reset.ts
+++ src/components/settingsMenu/advanced/reset.ts
@@ -1,8 +1,6 @@
 import { activeEntityTurnIndex, currentTheme, entities, isStarted } from "../../../store";
-
 export function reset() {
     entities.set([]);
-    currentTheme.set("stone");
     activeEntityTurnIndex.set(0);
     isStarted.set(false);
 }
