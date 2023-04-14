a0ae23d7c3d8116383772b14c335865a29d7f343
Edpacca <eddie@pace.me.uk>
2023-04-14 09:41:03 +0100

    add tooltip to log panel

--- src/components/logger/LogPanel.svelte
+++ src/components/logger/LogPanel.svelte
@@ -2,6 +2,7 @@
     import { fly } from "svelte/transition";
     import ArrowPlain from "../../graphics/icons/arrow-plain.svelte";
     import { isLogsOpen } from "../../store";
+    import Tooltip from "../common/tooltip/Tooltip.svelte";
     import LogEntries from "./LogEntries.svelte";
     const flyIn = {x: 214, duration: 400, opacity: 1};
 </script>
@@ -11,16 +12,20 @@
         <div in:fly={flyIn} out:fly={flyIn} class="panel-button-container">
             <LogEntries/>
             <div>
-                <button on:click={() => $isLogsOpen = false} class="close-arrow">
-                    <ArrowPlain/>
-                </button>
+                <Tooltip text="Close logs   Kw" type="help">
+                    <button on:click={() => $isLogsOpen = false} class="close-arrow">
+                        <ArrowPlain/>
+                    </button>
+                </Tooltip>
             </div>
         </div>
     {:else}
         <div in:fly={{x: 28, duration: 400, delay: 400}}>
-            <button on:click={() => $isLogsOpen = true} class="open-arrow">
-                <ArrowPlain class="left"/>
-            </button>
+            <Tooltip text="Open logs" type="help">
+                <button on:click={() => $isLogsOpen = true} class="open-arrow">
+                    <ArrowPlain class="left"/>
+                </button>
+            </Tooltip>
         </div>
     {/if}
 </div>
