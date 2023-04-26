c3e30e16048c8ea376874e28c8e60013675f899e
Edpacca <eddie@pace.me.uk>
2023-04-26 08:29:51 +0100

    initialise github pages

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'dist',
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/Edpacca/ednd-initiative.git',
+        user: {
+            name: 'Eddie Pace',
+            email: 'eddie@pace.me.uk'
+        }
+    },
+    () => {
+        console.log('Edpacca\'s initiative tracker deployed')
+    }
+)
