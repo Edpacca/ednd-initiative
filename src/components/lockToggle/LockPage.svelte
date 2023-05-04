e83bdb5c1febdf4be9b633dc9092c82ba0a0178d
Edpacca <eddie@pace.me.uk>
2023-05-05 00:01:27 +0100

    add logic to lock icon

--- src/components/lockToggle/LockPage.svelte
+++ src/components/lockToggle/LockPage.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import { isLocked, isLogsOpen, isStarted } from "../../store";
+    import { isLocked, isLogsOpen, isStarted, showTutorial } from "../../store";
     import LockedSwordsIcon from "./LockedSwordsIcon.svelte";
 
     const toggleLock = () => {
-        if (!$isStarted) {
+        if (!$isStarted && !$showTutorial) {
             $isStarted = true;
             // $isLogsOpen = true;
         }
